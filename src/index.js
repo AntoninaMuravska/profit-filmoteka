05a9db6b82c2911ab6dd4ea2c63b21fd4e0fc998
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-08-11 01:14:58 +0300

    удалил ересь

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './sass/main.scss';
-import './js/components/header';
-import './js/api/fetchApi.js';
-import './js/components/gallery.js';
-import './js/components/genre-transformator';
-import './js/components/modal';
-import './js/components/gallery.js';
 
+import refs from './js/components/refs';
+import onGalleryItemClick from './js/components/gallery';
+
+refs.filmCardRef.addEventListener('click',onGalleryItemClick);
