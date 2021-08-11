4bae6bbf1799faf6fa98de0f73ac6111552c7607
Galina-Syvak <77681699+Galina-Syvak@users.noreply.github.com>
2021-08-11 18:51:00 +0300

    session storage - film

--- src/js/components/session-storage.js
+++ src/js/components/session-storage.js
@@ -2,8 +2,14 @@ const saveGenres = function (key, genres) {
   sessionStorage.setItem(key, JSON.stringify(genres));
 };
 
-const getGenres = function (key) {
+const getItem = function (key) {
   return sessionStorage.getItem(key);
 };
 
-export { saveGenres, getGenres };
+//принимает масив обьектов фильмов, каждый фильм добавляет в sessionStorage
+//ключ - ID фильма, значение - весь обьект фильма (потом при запросе по ID фильма, можно получить обьект фильма)
+const saveFilmItem = function (array) {
+  array.map(elem => sessionStorage.setItem(elem.id, JSON.stringify(elem)));
+};
+
+export { saveGenres, getItem, saveFilmItem };
