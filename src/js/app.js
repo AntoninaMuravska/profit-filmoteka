d55621afdc1f13b24d889b39e219be40067402ae
Galina-Syvak <77681699+Galina-Syvak@users.noreply.github.com>
2021-08-16 15:08:57 +0300

    header fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
 import refs from './components/refs';
-import { onGalleryItemClick, makeGalleryFromLibraryItems, makeGalleryFromThrendesFilms, fetchGenres} from './components/gallery';
+import {
+  onGalleryItemClick,
+  makeGalleryFromLibraryItems,
+  makeGalleryFromThrendesFilms,
+  fetchGenres,
+} from './components/gallery';
 import { applyTheme, onChangeThemeSwitcherClick } from './components/theme';
 import { scrollUpInit } from './components/scrollup';
 import { onHomeBtnClick, onLibraryBtnClick } from './components/header';
 
-
 refs.gallery.addEventListener('click', onGalleryItemClick);
 refs.homeBtn.addEventListener('click', onHomeBtnClick);
 refs.libraryBtn.addEventListener('click', onLibraryBtnClick);
-
-
-
-
 
 // refs.homeBtn.addEventListener('click', makeGalleryFromThrendesFilms);
 // refs.queueBtn.addEventListener('click', makeGalleryFromLibraryItems);
@@ -24,14 +24,12 @@ refs.libraryBtn.addEventListener('click', onLibraryBtnClick);
 //   makeGalleryFromThrendesFilms(e);
 //   scrollUpInit();
 // });
-refs.homeBtn.addEventListener('click', makeGalleryFromThrendesFilms);
-refs.queueBtn.addEventListener('click', makeGalleryFromLibraryItems);
-refs.watchedBtn.addEventListener('click', makeGalleryFromLibraryItems);
+
 refs.themeSwitcher.addEventListener('change', onChangeThemeSwitcherClick);
 
 document.addEventListener('DOMContentLoaded', applyTheme);
 document.addEventListener('DOMContentLoaded', fetchGenres);
-window.addEventListener('load', (e) => {
+window.addEventListener('load', e => {
   // fetchGenres();
   makeGalleryFromThrendesFilms(e);
   scrollUpInit();
