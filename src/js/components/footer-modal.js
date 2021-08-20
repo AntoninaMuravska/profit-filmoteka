5b83e18c375b2065651cd9195ea0e9dcb583a657
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-08-20 10:15:48 +0300

    fix

--- src/js/components/footer-modal.js
+++ src/js/components/footer-modal.js
@@ -1,11 +1,19 @@
 import refs from './refs';
-import { scrollToHeader } from './scrollup';
+import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
+
 const closeBackdrop = document.querySelector('.footer-backdrop');
 const theme = document.querySelector('.theme-switch');
 
-export const closeOpenFooterModal = function (e) {
+
+export const closeOpenFooterModal = function () {
   refs.footerModal.classList.toggle('is-hidden');
   theme.classList.toggle('no-display');
   closeBackdrop.classList.toggle('is-hidden');
+  const isClosing = refs.footerModal.classList.contains('is-hidden');
+  if (isClosing) {
+    enableBodyScroll(document.body);
+  } else {
+    disableBodyScroll(document.body);
+  }
 };
 
